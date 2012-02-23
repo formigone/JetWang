ff49c607ae739f344537ef9e5ef108325ff89860
Mats Ramfjord <ramfjordm@gmail.com>
2012-02-23 18:03:25 +0100

    I dare you.

--- src/progark/a15/MainMenuActivity.java
+++ src/progark/a15/MainMenuActivity.java
@@ -10,6 +10,6 @@ public class MainMenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         //PlanetView planetView = (PlanetView)this.findViewById(R.id.planetView);
-        // http://www.youtube.com/watch?v=dQw4w9WgXcQ&ob=av3e
+        // http://bit.ly/aOVMt5
     }
 }
