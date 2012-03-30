89189ffd457170c36da494ef47bc49f80e3f2a8f
mads-b <mads@baardsgaard.com>
2012-03-30 16:31:33 +0200

    Update og Draw metodene til GameLayer

--- src/model/GameLayer.java
+++ src/model/GameLayer.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.ArrayList;
 
+import android.graphics.Canvas;
+
 
 public class GameLayer {
 	private ArrayList<Sprite> sprites = new ArrayList<Sprite>();
@@ -11,6 +13,16 @@ public class GameLayer {
 	}
 	public void removeSprite(Sprite s) {
 		sprites.remove(s);
+	}
+	public void update(float dt) {
+		for(Sprite s : sprites) {
+			s.update(dt);
+		}
+	}
+	public void draw(Canvas c) {
+		for(Sprite s : sprites) {
+			s.draw(c);
+		}
 	}
 
 }
