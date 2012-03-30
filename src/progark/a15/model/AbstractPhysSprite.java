95ef93889dc4b47ce72c25be75bb92ee1179a1c2
mads-b <mads@baardsgaard.com>
2012-03-30 18:34:37 +0200

    Liten optimalisering

--- src/progark/a15/model/AbstractPhysSprite.java
+++ src/progark/a15/model/AbstractPhysSprite.java
@@ -14,7 +14,7 @@ public abstract class AbstractPhysSprite implements CollisionListener,Sprite {
 	
 	public void update(float dt) {
 		//Move object according to equation of motion: r=r0+vt+at²/2
-		move(speed.x*dt+acceleration.x*dt*dt/2,
+		position.offset(speed.x*dt+acceleration.x*dt*dt/2,
 			 speed.y*dt+acceleration.y*dt*dt/2);
 		//Accellerate velocity (integration) v=v0+a*dt
 		speed.offset(acceleration.x*dt, acceleration.y*dt);
@@ -25,7 +25,6 @@ public abstract class AbstractPhysSprite implements CollisionListener,Sprite {
 	
 	public void setPosition(RectF r) { position = r; }
 	public RectF getPosition() { return position; }
-	public void move(float dx, float dy) { position.offset(dx, dy); }
 	//Some more physical parameters most sprites share.
 	public void setSpeed(PointF spd) { speed = spd; }
 	public PointF getSpeed() { return speed; }
