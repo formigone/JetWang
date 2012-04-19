3958426cbd61d13fc4e1f0ec2a995fc8fd4dfc72
mads-b <mads@baardsgaard.com>
2012-04-19 17:35:50 +0200

    Optimization n.2: Only do numerical integration if object actually has a speed or acceleration

--- src/progark/a15/model/AbstractPhysSprite.java
+++ src/progark/a15/model/AbstractPhysSprite.java
@@ -12,16 +12,22 @@ public abstract class AbstractPhysSprite implements CollisionListener,Sprite {
 	private RectF position = new RectF(0,0,0,0);
 	private PointF speed = new PointF(0,0);
 	private PointF acceleration = new PointF(0,0);	
-	
+
 	public void update(float dt) {
-		//Move object according to equation of motion: r=r0+vt+at²/2
-		position.offset(speed.x*dt+acceleration.x*dt*dt/2,
-						speed.y*dt+acceleration.y*dt*dt/2);
-		//Accelerate velocity (integration) v=v0+a*dt
-		speed.offset(acceleration.x*dt, acceleration.y*dt);
+		//Object has acceleration. Update speed.
+		if(acceleration.x!=0 || acceleration.y!=0) {
+			//Accelerate velocity (integration) v=v0+a*dt
+			speed.offset(acceleration.x*dt, acceleration.y*dt);
+		}
+		//Object has speed. update position
+		if(speed.x!=0 || speed.y!=0) {
+			//Move object according to equation of motion: r=r0+vt+at²/2
+			position.offset(speed.x*dt+acceleration.x*dt*dt/2,
+					speed.y*dt+acceleration.y*dt*dt/2);
+		}
 	}
-	
-	
+
+
 	public void setPosition(RectF r) { position = r; }
 	public void move(float dx,float dy) { position.offset(dx, dy); }
 	public RectF getPosition() { return position; }
@@ -33,5 +39,5 @@ public abstract class AbstractPhysSprite implements CollisionListener,Sprite {
 	public void setAcceleration(PointF acc) { acceleration=acc; }
 	public void setAcceleration(float x, float y) { acceleration.set(x, y); }
 	public PointF getAcceleration() { return acceleration; }
-	
+
 }
